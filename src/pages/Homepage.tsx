981337e60d8180ec0b9d72e7b3170ac3a94d07c6
alan890104 <alan890104@gmail.com>
2024-02-08 01:27:56 +0800

    refactor: Refactor TonConnect integration in Homepage component

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,50 +1,64 @@
-// import { TonConnectButton } from "@tonconnect/ui-react";
-import { useState } from "react";
-import { register } from "../api/user";
+import { useEffect } from "react";
+import {
+  TonConnectButton,
+  TonProofItemReplySuccess,
+  useTonConnectUI,
+  Account,
+} from "@tonconnect/ui-react";
+// import { useState } from "react";
+// import { register } from "../api/user";
 
 const HomePage = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [reason, setReason] = useState("");
-  const [link, setLink] = useState("");
-  const handleClick = async () => {
-    setIsLoading(true);
-    await register({ wallet_type: "tonkeeper" })
-      .then((res) => {
-        console.log(res);
-        setLink(res.data.url);
-      })
-      .catch((err) => {
-        setReason(err.message);
-        setTimeout(() => {
-          setReason("");
-        }, 2000);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+  const [tonConnectUI] = useTonConnectUI();
+
+  // enable ui loader
+  tonConnectUI.setConnectRequestParameters({ state: "loading" });
+
+  const fetchTonProofPayloadFromBackend = () => {
+    return JSON.stringify({
+      telegram_id: "1234567890",
+    });
   };
+
+  const checkProofInYourBackend = (
+    proof: TonProofItemReplySuccess,
+    account: Account,
+  ) => {
+    console.log("proof", proof);
+    console.log("account", account);
+  };
+
+  useEffect(
+    () =>
+      tonConnectUI.onStatusChange((wallet) => {
+        if (!wallet) return;
+        if (
+          wallet.connectItems?.tonProof &&
+          "proof" in wallet.connectItems.tonProof
+        ) {
+          checkProofInYourBackend(wallet.connectItems.tonProof, wallet.account);
+        }
+      }),
+    [tonConnectUI],
+  );
+
+  // fetch you tonProofPayload from the backend
+  const tonProofPayload: string | null = fetchTonProofPayloadFromBackend();
+
+  if (!tonProofPayload) {
+    // remove loader, connect request will be without any additional parameters
+    tonConnectUI.setConnectRequestParameters(null);
+  } else {
+    // add tonProof to the connect request
+    tonConnectUI.setConnectRequestParameters({
+      state: "ready",
+      value: { tonProof: tonProofPayload },
+    });
+  }
   return (
-    <div className="relative flex h-full w-full flex-col items-center justify-center gap-10">
-      <button
-        className=" flex justify-center rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
-        onClick={handleClick}
-      >
-        {isLoading ? "Loading..." : "Register"}
-      </button>
-      {reason && (
-        <div className="absolute bottom-0 bg-red-500 p-2 text-white">
-          {reason}
-        </div>
-      )}
-      {link && (
-        <a
-          href={link}
-          className="flex h-10 w-20 items-center justify-center bg-green-500"
-        >
-          link
-        </a>
-      )}
-    </div>
+    <>
+      <TonConnectButton />
+    </>
   );
 };
 
